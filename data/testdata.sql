f4695d9b4a883bf8989d2d37f8fc4c79b9093000
Florin <florinvisla@gmail.com>
2026-03-11 17:22:45 +0200

    feat: added encryption for basic auth passwords

--- data/testdata.sql
+++ data/testdata.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
                        username VARCHAR(50) NOT NULL,
-                       password VARCHAR(50) NOT NULL,
+                       password VARCHAR(68) NOT NULL,
                        enabled BOOLEAN NOT NULL,
                        PRIMARY KEY (username)
 );
